9461d5713906fd3f00a5a0a4628a2b62b85fe965
antonio <aorizondoleyva@gmail.com>
2023-04-26 15:14:48 -0400

    Update tests.requirements.txt

--- tests/app_settings.py
+++ tests/app_settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = (
     "django.contrib.messages",
     "django.contrib.sessions",
     "django.contrib.staticfiles",
-    "django_json_widget",
     "telegram_django_bot",
     "test_app",
 )
